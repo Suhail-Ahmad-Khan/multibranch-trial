748eceedf8e911bdd3b9cf8f0ee8551cc00b3156
khansuhl <khansuhl@gmail.com>
2018-08-06 15:38:04 +0530

    first run

---
+++ src/main/java/com/example/learning/SpringbootHelloworldJarCodeDeploy1Application.java
@@ -0,0 +1,12 @@
+package com.example.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldJarCodeDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldJarCodeDeploy1Application.class, args);
+	}
+}
